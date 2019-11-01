bdddce5d2d4e401517e9ff5144f501f40d4167f9
jeremymsmith <jeremy79@ksu.edu>
2019-10-31 20:12:25 -0500

    Tweaked gradient boost and modified script for making predictions onto kaggle test data frame.

--- Titanic_Work.R
+++ Titanic_Work.R
@@ -10,6 +10,7 @@ library(boot)
 library(tidyverse)
 library(caret)
 library(ggplot2)
+library(gbm)
 
 #Script that adds features to training data
 #Stores featured data as "full"
@@ -20,22 +21,42 @@ full <- full%>%mutate(HasCabin=(ifelse(Cabin=="",0,1)))%>%
   mutate(Child=ifelse(FilledAge<16,TRUE,FALSE))
 full$Embarked <- replace(full$Embarked,which(is.na(full$Embarked)),"S")
 full$Survived <- full$Survived%>%as.factor()
+full$Child <- full$Child%>%as.factor()
 full$Age <- NULL
 full$Ticket <- NULL
 full$Name <- NULL
 full$Cabin <- NULL
 
 #Creating a training and testing subset randomly with features from full
+set.seed(1)
 training.samples <- full$Survived%>%
-  createDataPartition(p=.25,list=FALSE)
+  createDataPartition(p=.8,list=FALSE)
 full_train <- full[training.samples,]
 full_test <- full[-training.samples,]
 
 #Trying a random forest. 
-rf.full_train <- randomForest(Survived~Pclass+Sex+Famille+Child,data=full_train,importance=TRUE)
+#rf.full_train <- randomForest(Survived~Pclass+Sex+Famille+Child,data=full_train,importance=TRUE)
 
 #Testing random forest on full_test
-mean(predict(rf.full_train,full_test)==full_test$Survived)
+#mean(predict(rf.full_train,full_test)==full_test$Survived)
+
+#Trying a boost.
+boost.full_train <-  gbm(Survived ~.,
+              data = full_train,
+              distribution = "multinomial",
+              cv.folds=10,
+              shrinkage = .01,
+              n.minobsinnode = 10,
+              n.trees = 5000)
+
+#Testing boost on full_test
+pred.full_train <-  predict.gbm(object = boost.full_train,
+                   newdata = full_test,
+                   n.trees = 5000,
+                   type = "response")
+Survived.pred <-  colnames(pred.full_train)[apply(pred.full_train, 1, which.max)]
+full_test <- data.frame(full_test,Survived.pred)
+mean(full_test$Survived==full_test$Survived.pred)
 
 #Imported test into global environment. 
 #Creating "final" for submission to kaggle
